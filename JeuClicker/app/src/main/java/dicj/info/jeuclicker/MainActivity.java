5413d557e53eaa28d87a84a46bf62241b8d3e68e
lordfred23 <fred.cote23@gmail.com>
2017-01-31 14:38:52 -0500

    onClicky Fonctionnel

--- JeuClicker/app/src/main/java/dicj/info/jeuclicker/MainActivity.java
+++ JeuClicker/app/src/main/java/dicj/info/jeuclicker/MainActivity.java
@@ -3,15 +3,55 @@ package dicj.info.jeuclicker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    int test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         UiChangeListener();
+        hideSystemUI();
+        test=0;
+
     }
+    public void onClicky(View v)
+    {
+
+        switch (v.getId())
+        {
+            case R.id.layClick:test+=1;
+                break;
+        }
+
+        TextView txtGold = (TextView)findViewById(R.id.idGold);
+        txtGold.setText(""+test);
+
+
+    }
+
+
+
+
+    private void hideSystemUI() {
+        View mDecorView= getWindow().getDecorView();
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
     public void UiChangeListener()
     {
         final View decorView = getWindow().getDecorView();
