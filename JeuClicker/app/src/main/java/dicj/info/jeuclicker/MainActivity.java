596223142e0f019087f00c922111e7ef7209b604
lordfred23 <fred.cote23@gmail.com>
2017-02-21 13:57:18 -0500

    ajout du systeme dachat de skill

--- JeuClicker/app/src/main/java/dicj/info/jeuclicker/MainActivity.java
+++ JeuClicker/app/src/main/java/dicj/info/jeuclicker/MainActivity.java
@@ -7,19 +7,20 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.os.Handler;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    int test,level,damage,kill;
-    double life,lifeAfficher;
+    int test, level, damage, kill;
+    double life, lifeAfficher;
     boolean start;
     private Handler progressBarHandler = new Handler();
     CMonstre monster;
 
 
-     ProgressBar pg;
-     TextView txtGold,txtLife,txtLevel,txtKill;
-
+    ProgressBar pg;
+    TextView txtGold, txtLife, txtLevel, txtKill;
 
 
     @Override
@@ -28,31 +29,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         UiChangeListener();
         hideSystemUI();
-        test=0;
-        life=100;
-        level=1;
-        pg =(ProgressBar)findViewById(R.id.myProgress);
-        txtGold = (TextView)findViewById(R.id.idGold);
-        txtLife = (TextView)findViewById(R.id.txtLife);
-        txtLevel=(TextView)findViewById(R.id.txtLevel);
-        txtKill= (TextView)findViewById(R.id.kill);
-        start=true;
+        test = 0;
+        life = 100;
+        level = 1;
+        pg = (ProgressBar) findViewById(R.id.myProgress);
+        txtGold = (TextView) findViewById(R.id.idGold);
+        txtLife = (TextView) findViewById(R.id.txtLife);
+        txtLevel = (TextView) findViewById(R.id.txtLevel);
+        txtKill = (TextView) findViewById(R.id.kill);
+        start = true;
         createMonster(1);
-        damage=1;
-        kill=0;
-        lifeAfficher=100;
+        damage = 1;
+        kill = 0;
+        lifeAfficher = 100;
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 {
 
-                        txtGold.setText("" + test);
-                        txtLife.setText(""+monster.getLife());
-                        pg.setProgress( (int)lifeAfficher);
-                        txtLevel.setText(""+level);
-                        txtKill.setText(""+kill);
-                        progressBarHandler.postDelayed(this, 300);
+                    txtGold.setText("" + test);
+                    txtLife.setText("" + monster.getLife());
+                    pg.setProgress((int) lifeAfficher);
+                    txtLevel.setText("Level :" + level);
+                    txtKill.setText(" nb de Kill :" + kill);
+                    progressBarHandler.postDelayed(this, 300);
 
                 }
             }
@@ -60,69 +61,81 @@ public class MainActivity extends AppCompatActivity {
         progressBarHandler.post(runnable);
 
 
-
-
-
-
     }
+
     public void onClicky(View v) {
 
 
-        switch (v.getId())
-        {
-            case R.id.layClick:jouer();
-
+        switch (v.getId()) {
+            case R.id.layClick:
+                jouer();
+                break;
+            case R.id.attack1:
+                if (enoughGold(25))
+                    damage++;
 
                 break;
         }
 
     }
-    private void jouer()
-    {
-        if (monster.getLife()<=0)
-        {
-            lifeAfficher=100;
+
+    private void jouer() {
+        if (monster.getLife() <= 0) {
+            lifeAfficher = 100;
 
             kill++;
-            if(kill % 5==0)
+            if (kill % 5 == 0) {
+                randomGold();
                 createMonster(level++);
-            else
+            } else {
                 createMonster(level);
+                randomGold();
+            }
+
         }
         Attack();
-        if (monster.getLife()<=0)
-        {
-            lifeAfficher=100;
+        if (monster.getLife() <= 0) {
+            lifeAfficher = 100;
 
             kill++;
-            if(kill % 5==0)
+            if (kill % 5 == 0) {
+                randomGold();
                 createMonster(level++);
-            else
+            } else {
                 createMonster(level);
+                randomGold();
+            }
+
         }
 
 
     }
-    private void createMonster(int level){
+
+    private void createMonster(int level) {
 
 
-            monster = new CMonstre(level * 10, false);
-            post(monster.getLife());
+        monster = new CMonstre(level * 10, false);
+        post(monster.getLife());
     }
+
     private void Attack() {
-        monster.setLife(monster.getLife()-damage);
-        test+=1;
-        lifeAfficher=(double)monster.getLife()/life;
+        monster.setLife(monster.getLife() - damage);
+        lifeAfficher = (double) monster.getLife() / life;
     }
+
     private void post(int life) {
-       this.life = (double)monster.getLife()/100;
+        this.life = (double) monster.getLife() / 100;
 
     }
 
+    private void randomGold() {
+        Random rn = new Random();
+        test += rn.nextInt(10) * 1.5;
+    }
 
 
     private void hideSystemUI() {
-        View mDecorView= getWindow().getDecorView();
+        View mDecorView = getWindow().getDecorView();
         // Set the IMMERSIVE flag.
         // Set the content to appear under the system bars so that the content
         // doesn't resize when the system bars hide and show.
@@ -135,15 +148,14 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
-    public void UiChangeListener()
-    {
+    public void UiChangeListener() {
         final View decorView = getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
             @Override
             public void onSystemUiVisibilityChange(int visibility) {
                 if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
                     decorView.setSystemUiVisibility(
-                                      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -154,6 +166,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private boolean enoughGold(int cost) {
+        if (cost <= test) {
+            test = test - cost;
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
 }
+
+
 
 
